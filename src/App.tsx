250f4164285e1415128e77547347ce32079394c0
Victor Hugo Freitas Magalhães <vmagalhaes@brex.com>
2025-02-19 17:13:21 -0300

    chore: Actually do repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { graphql } from "relay-runtime";
 
-function App() {
-  const [count, setCount] = useState(0)
+// export gql nodes isn't idiomatic but we're doing this just to appease the unused variables lint rule
+export const query = graphql`
+    query AppQuery($showEmail: Boolean!) @raw_response_type {
+        ...AppFragment
+        ...AppConditionalFragment
+    }
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+export const fragment = graphql`
+    fragment AppFragment on Query {
+        account {
+            name
+        }
+    }
+`;
 
-export default App
+export const conditionalFragment = graphql`
+    fragment AppConditionalFragment on Query {
+        account @include(if: $showEmail) {
+            email
+        }
+    }
+`;
